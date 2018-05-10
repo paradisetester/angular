04d1972e1b1f115c17a04292f72d9ab52577d94f
Paradise <paradisetester@gmail.com>
2018-05-10 17:07:54 +0530

    angular 4 todo

---
+++ angular-todo/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	providers: [TodoDataService]
+})
+export class AppComponent {
+	
+	newTodo: Todo = new Todo();
+	checkall = false;
+	
+	optionsChecked = [];
+	optionsMap  = [];
+	options = {};
+	
+	
+	constructor(private todoDataService: TodoDataService) {
+	}
+	
+	addTodo() {
+		this.todoDataService.addTodo(this.newTodo);
+		this.newTodo = new Todo();
+	}
+	
+	toggleTodoComplete(todo) {
+		this.todoDataService.toggleTodoComplete(todo);
+	}
+	
+	
+	removeTodo(todo) {
+		this.todoDataService.deleteTodoById(todo.id);
+	}
+	
+	get todos() {
+		return this.todoDataService.getAllTodos();
+	}
+	
+}
---
+++ angular-todo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ChecklistModule } from 'angular-checklist';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		ChecklistModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
